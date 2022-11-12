3d589c0678878989e2830dc678233661cd497e98
OmriHadad747 <omrihadad747@gmail.com>
2022-11-13 00:09:39 +0200

    no msg

---
+++ database/freelancers.py
@@ -0,0 +1,151 @@
+from typing import Dict, List, Optional, Any
+from pymongo import command_cursor
+from pymongo.operations import UpdateOne
+from bson import ObjectId
+from models import freelancer as freelancer_model
+from models import job as job_moedl
+from database import db, _freelancers
+
+
+class FreelancerDatabase:
+
+    @classmethod
+    def find(cls, job: job_moedl.Job) ->  command_cursor.CommandCursor:
+        # TODO write docstring
+        # TODO log here
+        freelancers = db[_freelancers].aggregate([
+            {
+                "$geoNear": {
+                    "near": {
+                        "type": "Point",
+                        "coordinates": [job.customer_lon, job.customer_lat]
+                    },
+                    "spherical": True,
+                    "query": {
+                        "current_status": freelancer_model.FreelancerStatusEnum.AVAILABLE.value,
+                        "county": job.customer_county
+                    },
+                    "distanceField": "distance"
+                }
+            }
+        ])
+        # TODO log here
+        yield freelancers
+
+    
+    @classmethod
+    def get_freelancer_by_id(cls, id: str) -> Optional[Any]:
+        freelancer = db[_freelancers].find_one({"_id": ObjectId(id)})
+        return freelancer
+
+
+    @classmethod
+    def get_freelancer_by_email(cls, email: str) -> Optional[Any]:
+        freelancer = db[_freelancers].find_one({"email": email})
+        return freelancer
+
+
+    @classmethod
+    def add_freelancer(cls, freelancer: Dict[str, Any]) -> Optional[bool]:
+        result = db[_freelancers].insert_one(freelancer)
+        return result.acknowledged
+
+
+    @staticmethod
+    def _build_array_update_write(field: str, op: str, data: List[Any]) -> Optional[Dict[str, Any]]:
+        """
+        - building 'update' argument for PyMongo's UpdateOne method 
+        for document fields that are an array type
+
+        Args:
+            - field (str): document's field name to update
+            - op (str): operation to perform. available options: ['add', 'rem']
+            - data (List[Any]): data to add or remove from the document's field
+
+        Raises:
+            - ValueError: in case of unknown/unsupported 'op' (operation)
+
+        Returns:
+            - Optional[Dict[str, Any]]: dictionary that can be rendered by MongoDB driver
+        """
+        if op == "add":
+            return {
+                "$addToSet": {
+                    field: {
+                        "$each": data
+                    }
+                }
+            }
+        elif op == "rem":
+            return {
+                "$pull": {
+                    field: {
+                        "$in": data
+                    }
+                }
+            }
+        else:
+            raise ValueError(f"unknown operation - {op}")
+
+
+    @staticmethod
+    def _adapt_for_bulkwrite(fields_to_update: Dict[str, Any], _filter: Dict[str, Any]) -> Optional[List[Dict[str, Any]]]:
+        """
+        - iterate through all the provided document fields and build a list of updates
+        to perform using the 'bulk_write' method
+
+        - in case a field is an array type, use '_build_array_update_write' static function
+        to create an 'update' object appropriate for an array
+
+        Args:
+            - fields_to_update (Dict[str, Any]): bunch of fields to update in a document
+            - _filter (Dict[str, Any]): filter to apply on the updates
+
+        Raises:
+            - ValueError: 
+                - in case an array field doesn't have kind of operation ['add', 'rem'] to perform
+                - in case an array field contains more then 1 operation ['add', 'rem'] to perform
+
+        Returns:
+            - Optional[List[Dict[str, Any]]]: list of updateOne operation to perform as part of the 'bulk_write' method
+        """
+        writes = []
+        
+        basic_update_write = {"$set": {
+
+        }}
+
+        for field, val in fields_to_update.items():
+            if isinstance(val, dict):
+                if len(val) == 0 or len(val) > 1:
+                    raise ValueError(f"'{field}' can't be empty or contain more then 1 key/operation ['add', 'rem']")
+
+                op, data = val.popitem()
+                writes.append(
+                    UpdateOne(_filter, FreelancerDatabase._build_array_update_write(field, op, data))
+                )
+            else:
+                basic_update_write["$set"][field] = val
+
+        # append the '$set' update operation at the end of the list so it will be 
+        # executed last, such that in case the user's email is a part of the 
+        # update, filtering by user's email will not work (cause the user's email changed)       
+        if len(basic_update_write.get("$set")) > 0:
+            writes.append(
+                UpdateOne(_filter, basic_update_write)
+            )
+
+        return writes
+
+
+    @classmethod
+    def update_freelancer(cls, email: str, fields: Dict[str, Any]) -> Optional[bool]:
+        writes = cls._adapt_for_bulkwrite(fields, _filter={"email": email})
+        result = db[_freelancers].bulk_write(writes)
+        return result.acknowledged
+
+
+    @classmethod
+    def delete_freelancer(cls, email: str) -> Optional[bool]:
+        result = db[_freelancers].delete_one({"email": email})
+        return result.acknowledged
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from functools import reduce
+from setuptools.command.install import install
+import os
+
+
+class PostInstall(install):
+    def __init__(self, *args, **kwargs):
+        super(PostInstall, self).__init__(*args, **kwargs)
+        _install_requirements()
+
+
+def _install_requirements():
+    os.system("pre-commit install")
+
+
+REQUIRED_PACKAGES = [
+    "Flask-PyMongo==2.3.0"
+    "pydantic==1.10.2"
+    "pymongo==4.3.2"
+]
+
+EXTRA_REQUIREMENTS = {}
+
+name = "skip-db-lib"
+version = "1.0.0"
+description = ("A modular library that exposes a set of database operation")
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    python_requires=">=3.8",
+    packages=["res", *find_packages(include=["hemunah_core*"])],
+    package_data={"": ["res/*.csv", "res/*.json", "res/*.yaml"]},
+    include_package_data=True,
+    install_requires=REQUIRED_PACKAGES,
+    extras_require={
+        **EXTRA_REQUIREMENTS,
+        "all": reduce(lambda agg, value: agg + value, EXTRA_REQUIREMENTS.values(), []),
+    },
+    cmdclass={"install": PostInstall},
+)
