2e482aad73c137122f05c6394dd73090d8319027
OmriHadad747 <omrihadad747@gmail.com>
2022-11-13 14:56:38 +0200

    fix setup file

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ def _install_requirements():
 
 
 REQUIRED_PACKAGES = [
-    "Flask-PyMongo==2.3.0"
-    "pydantic==1.10.2"
-    "pymongo==4.3.2"
+    "Flask-PyMongo == 2.3.0"
+    "pydantic == 1.10.2"
+    "pymongo == 4.3.2"
 ]
 
 EXTRA_REQUIREMENTS = {}
