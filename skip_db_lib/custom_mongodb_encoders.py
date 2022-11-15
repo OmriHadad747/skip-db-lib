59a8acce6cb604fbb12bf12292da25d8b69640cd
OmriHadad747 <omrihadad747@gmail.com>
2022-11-15 22:07:10 +0200

    encoder for freelancer status added

--- skip_db_lib/custom_mongodb_encoders.py
+++ skip_db_lib/custom_mongodb_encoders.py
@@ -1,5 +1,6 @@
 from bson.codec_options import TypeEncoder, TypeRegistry, CodecOptions
 from skip_db_lib.models.job import JobCategoryEnum, JobStatusEnum
+from skip_db_lib.models.freelancer import FreelancerStatusEnum
 
 
 class JobCategoryEncoder(TypeEncoder):
@@ -26,7 +27,20 @@ class JobStatusEncoder(TypeEncoder):
         return value.value
 
 
+class FreelancertatusEncoder(TypeEncoder):
+
+    python_type = FreelancerStatusEnum  # the Python type acted upon by this type codec
+
+    def transform_python(self, value):
+        """
+        Function that transforms a custom type value into a type
+        that BSON can encode.
+        """
+        return value.value
+
+
 job_category_encoder = JobCategoryEncoder()
 job_status_encoder = JobStatusEncoder()
-type_registry = TypeRegistry([job_category_encoder, job_status_encoder])
+freelancer_status_encoder = FreelancertatusEncoder()
+type_registry = TypeRegistry([job_category_encoder, job_status_encoder, freelancer_status_encoder])
 codec_options = CodecOptions(type_registry=type_registry)
