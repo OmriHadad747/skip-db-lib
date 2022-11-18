0e5728cf4d6edbb902913bcbc663d32b5dc24832
OmriHadad747 <omrihadad747@gmail.com>
2022-11-18 12:28:46 +0200

    a

--- skip_db_lib/models/job.py
+++ skip_db_lib/models/job.py
@@ -38,7 +38,7 @@ class Job(CustomBaseModel):
     freelancer_email: str = None
     freelancer_phone: str = None
 
-    @pyd.validator("_id")
+    @pyd.validator("id")
     def convert_id_to_str(cls, value):
         if isinstance(value, ObjectId):
             return str(value)
